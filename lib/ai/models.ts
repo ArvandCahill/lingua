c261006a2d0a7f9b44ff1226ed0d19df8637e383
Arvand Cahil <arvand.ganteng.06@gmail.com>
2024-12-02 13:30:52 +0700

    Change AI Model

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -5,21 +5,21 @@ export interface Model {
   label: string;
   apiIdentifier: string;
   description: string;
-}
+} 
 
 export const models: Array<Model> = [
   {
-    id: 'gpt-4o-mini',
-    label: 'GPT 4o mini',
-    apiIdentifier: 'gpt-4o-mini',
+    id: 'gemini-1.5-flash',
+    label: 'gemini-1.5-flash',
+    apiIdentifier: 'gemini-1.5-flash',
     description: 'Small model for fast, lightweight tasks',
   },
   {
-    id: 'gpt-4o',
-    label: 'GPT 4o',
-    apiIdentifier: 'gpt-4o',
+    id: 'gemini-1.5-pro',
+    label: 'gemini-1.5-pro',
+    apiIdentifier: 'gemini-1.5-pro',
     description: 'For complex, multi-step tasks',
   },
 ] as const;
 
-export const DEFAULT_MODEL_NAME: string = 'gpt-4o-mini';
+export const DEFAULT_MODEL_NAME: string = 'gemini-1.5-pro';
